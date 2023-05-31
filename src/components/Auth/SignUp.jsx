f885c1722152fe73feba604f30ce21b952a142d7
errorby <mdidayev@inbox.ru>
2023-05-31 11:55:58 +0300

    ревью страницы регистрации

--- src/components/Auth/SignUp.jsx
+++ src/components/Auth/SignUp.jsx
@@ -4,11 +4,18 @@ import { useDispatch } from 'react-redux';
 import { authSignUp } from '../../features/applicationSlice';
 import Header from '../Header/Header';
 import styles from './Auth.module.css'
+import { useNavigate } from 'react-router-dom';
 
 const SignUp = () => {
-    const [login, setLogin] = useState('')
-    const [password, setPassword] = useState('')
-    const dispatch = useDispatch()
+    const [login, setLogin] = useState('');
+    const [password, setPassword] = useState('');
+
+    const [email, setEmail] = useState('');
+    const [firstName, setFirstName] = useState('');
+    const [surName, setSurName] = useState('');
+
+    const dispatch = useDispatch();
+    const navigate = useNavigate();
 
     const handleSetName = (e) => {
         setLogin(e.target.value);
@@ -19,8 +26,12 @@ const SignUp = () => {
     }
 
     const handleSignUp = (e) => {
-        e.preventDefault();
+        e.preventDefault()
         dispatch(authSignUp({ login, password }))
+        setEmail('');
+        setFirstName('');
+        setSurName('');
+        navigate('/signin')
     }
 
     return (
@@ -28,36 +39,70 @@ const SignUp = () => {
             <Header />
             <div className={styles.login}>
                 <div className={styles.loginScreen}>
-                    <div class={styles.appTitle}>
+                    <div className={styles.appTitle}>
                         <h1>Создать аккаунт</h1>
                     </div>
-                    <div class={styles.loginForm}>
-                        <div class={styles.controlGroup}>
-                            <form onSubmit={handleSignUp}>
+                    <div className={styles.loginForm}>
+                        <div className={styles.controlGroup}>
+                            <form>
                                 <input
-                                    id="login-name"
-                                    class={styles.loginField}
+                                    className={styles.loginField}
+                                    type='text'
+                                    value={firstName}
+                                    placeholder='Имя'
+                                    onChange={(e) => { setFirstName(e.target.value) }}
+                                />
+                            </form>
+                        </div>
+                        <br />
+                        <div className={styles.controlGroup}>
+                            <form>
+                                <input
+                                    className={styles.loginField}
+                                    type='text'
+                                    value={surName}
+                                    placeholder='Фамилия'
+                                    onChange={(e) => { setSurName(e.target.value) }}
+                                />
+                            </form>
+                        </div>
+                        <br />
+                        <div className={styles.controlGroup}>
+                            <form>
+                                <input
+                                    className={styles.loginField}
+                                    type='text'
+                                    value={email}
+                                    placeholder='Почта'
+                                    onChange={(e) => { setEmail(e.target.value) }}
+                                />
+                            </form>
+                        </div>
+                        <br />
+                        <div className={styles.controlGroup}>
+                            <form>
+                                <input
+                                    className={styles.loginField}
                                     type='text'
                                     value={login}
-                                    placeholder='логин'
+                                    placeholder='Логин'
                                     onChange={handleSetName}
                                 />
                             </form>
                         </div>
                         <br />
-                        <div class={styles.controlGroup}>
+                        <div className={styles.controlGroup}>
                             <form>
                                 <input
-                                    id="login-pass"
-                                    class={styles.loginField}
+                                    className={styles.loginField}
                                     type='password'
                                     value={password}
-                                    placeholder='пароль'
+                                    placeholder='Пароль'
                                     onChange={handleSetPass}
                                 />
                                 <br />
-                                <button class={styles.btn} type='submit' onClick={handleSignUp}>Зарегистрироваться</button>
-                                <a class={styles.loginLink} href="#">У вас есть аккаунт?</a>
+                                <button className={styles.btn} type='submit' onClick={handleSignUp}>Зарегистрироваться</button>
+                                <a className={styles.loginLink} href="#">У вас есть аккаунт?</a>
                             </form>
                         </div>
                     </div>
