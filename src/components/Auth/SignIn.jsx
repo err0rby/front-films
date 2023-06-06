1cd671c49b82132cc7fe1fa91785ac627b449d0a
errorby <mdidayev@inbox.ru>
2023-06-06 16:12:27 +0300

    Орф. правка

--- src/components/Auth/SignIn.jsx
+++ src/components/Auth/SignIn.jsx
@@ -41,7 +41,7 @@ const SignIn = () => {
                                     className={styles.loginField}
                                     type='text'
                                     value={login}
-                                    placeholder='логин'
+                                    placeholder='Логин'
                                     onChange={handleSetName}
                                 />
                             </form>
@@ -54,7 +54,7 @@ const SignIn = () => {
                                     className={styles.loginField}
                                     type='password'
                                     value={password}
-                                    placeholder='пароль'
+                                    placeholder='Пароль'
                                     onChange={handleSetPass}
                                 />
                                 <br />
